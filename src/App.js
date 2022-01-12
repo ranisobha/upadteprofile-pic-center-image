be2c5ad9d7d54f1ba2e5bb36519453e598e38587
ranisobha <ranisobha@gmail.com>
2022-01-12 07:09:11 +0530

    sample pro

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from './components/Title';
+import Profile from './components/Profile';
+const App = () =>
+{
+  return (
+    <div>
+      <Title />
+      <Profile />
+    </div>
+  )
+}
+
+export default App;
